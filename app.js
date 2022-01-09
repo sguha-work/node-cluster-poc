3e9162c423d53bbd2ad7b9716edc1c3afc59fdc9
Aritrik Das <aritrikdas@gmail.com>
2022-01-09 10:52:38 +0530

    classic express example

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const app = express();
+app.get('/', (req, res) => {
+  for(let counter=0; counter<1e8; counter++){}
+  res.send('hello world');
+})
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+const cluster = require('cluster');
+const os = require('os');
+const numberOfCPUs = os.cpus().length;
+//console.log(`Number of CPU ${numberOfCPUs}`);
+if (cluster.isMaster) {
+  for (let counter = 0; counter < numberOfCPUs; counter++) {
+    cluster.fork();
+  }
+  cluster.on('exit',(worker, code, signal)=>{
+    console.log(`worker with pid ${worker.process.pid} died`);
+    cluster.fork();
+  })
+} else {
+  app.listen(3000, () => {
+    console.log(`server ${process.pid} listing on port 3000`);
+  });
+}
